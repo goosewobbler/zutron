f6e014df7e74a3f7271ff8433b134079c053bf35
Sam Maister <goosewobbler@protonmail.com>
2024-10-11 15:14:29 +0100

    fix: use correct bridge

--- apps/example-reducers/src/renderer/hooks/useStore.ts
+++ apps/example-reducers/src/renderer/hooks/useStore.ts
@@ -1,4 +1,4 @@
 import { createUseStore } from 'zutron';
 import type { State } from '../../features/index.js';
 
-export const useStore = createUseStore<State>(window.electron);
+export const useStore = createUseStore<State>(window.zutron);
