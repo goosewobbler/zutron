1267c7f81c6312302ddd8c2867a24d086ef81e38
goosewobbler <goosewobbler@protonmail.com>
2024-11-06 15:19:02 +0000

    chore: remove unused type

--- packages/zutron/test/preload.spec.ts
+++ packages/zutron/test/preload.spec.ts
@@ -1,4 +1,4 @@
-import { vi, expect, describe, it, type Mock, beforeEach } from 'vitest';
+import { vi, expect, describe, it, beforeEach } from 'vitest';
 import { createIpcRendererMock } from './helpers.js';
 
 const mockIpcRenderer = createIpcRendererMock();
